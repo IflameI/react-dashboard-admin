c94154d41c20b286ca8c924c14d7dcc1858910fe
iflamei <iflamei@mail.ru>
2021-05-14 14:26:27 +0300

    Final Version 2.0. Deploy heroku

--- server.js
+++ server.js
@@ -10,4 +10,6 @@ app.use('/login', (req, res) => {
   });
 });
 
-app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+app.listen(process.env.PORT || 8080, () =>
+  console.log('API is running on http://localhost:8080/login'),
+);
