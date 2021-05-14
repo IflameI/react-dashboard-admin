3f3d94953f340b81b09fe1d6cade7477cf6431dd
iflamei <iflamei@mail.ru>
2021-05-14 16:36:36 +0300

    Final Version 2.0. Deploy heroku

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 app.use(cors());
-
-app.use(express.static('public'));
 
 app.use('/login', (req, res) => {
   res.send({
@@ -12,6 +11,13 @@ app.use('/login', (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || 8080, () =>
-  console.log('API is running on http://localhost:8080/login'),
-);
+if (process.env.NODE_ENV === 'production') {
+  // Обслуживаем любые статические файлы
+  app.use(express.static(path.join(__dirname, 'client / build')));
+  // Обработка маршрутизации React, возврат всех запросов в приложение React
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client / build', 'index.html'));
+  });
+}
+
+app.listen(process.env.PORT || 8080, () => console.log('API is running'));
