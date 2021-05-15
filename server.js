a2894e3689956f6e8e21f0eb43a06ff057fbe4e4
iflamei <iflamei@mail.ru>
2021-05-15 10:04:05 +0300

    Final version 3.0. Все еще деплой на heroku

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
-app.use(cors());
+const cors = require('cors');
 
 app.use('/login', (req, res) => {
   res.send({
     token: 'test123',
   });
 });
-
+const whitelist = [
+  'http://localhost:3000',
+  'http://localhost:8080',
+  'https://dashboard-react-admin.herokuapp.com/',
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log('** Origin of request ' + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log('Origin acceptable');
+      callback(null, true);
+    } else {
+      console.log('Origin rejected');
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+app.use(cors(corsOptions));
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
